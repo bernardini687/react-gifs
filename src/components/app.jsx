570328673b8a34e42184f0304ccc7bf89e0a7f6a
Oscar Bernardini <bernardini687@gmail.com>
2019-05-05 10:24:46 +0200

    Render App component on page

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className="left-scene"></div>
+//         <div className="right-scene"></div>
+//       </div>
+//     );
+//   }
+// }
 
-}
+const App = () => {
+  return (
+    <div>
+      <div className="left-scene" />
+      <div className="right-scene" />
+    </div>
+  );
+};
+
+export default App;
