230b0328c6ceb051d628b4ab5405b1eb7745e052
Oscar Bernardini <bernardini687@gmail.com>
2019-05-07 09:10:11 +0200

    Change GifList from class to function

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import Gif from './gif';
 
-class GifList extends Component {
-  renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
-  }
-
-  render() {
-    return (
-      <div className="gif-list">
-        {this.renderList()}
-      </div>
-    );
-  }
-}
+const GifList = (props) => {
+  const { gifs, selectGif } = props;
+  return (
+    <div className="gif-list">
+      {gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={selectGif} />)}
+    </div>
+  );
+};
 
 export default GifList;
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
 import App from './components/app';
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+ReactDOM.render(<App />, document.getElementById('root'));
